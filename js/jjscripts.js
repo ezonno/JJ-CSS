dfd85f3448612a55c034b91117ab40a279e0e556
jeppeolesen <jeppe.olesen@bsmac0073.bestcorp.net>
2013-11-25 10:54:14 +0100

    Updated hax to fix promos

--- js/jjscripts.js
+++ js/jjscripts.js
@@ -7,6 +7,13 @@ var jjShizzle = (function (jQuery) {
 	return {
 
 		// custom functions here
+			wrapPromos : function () {
+				$('#branded .content .wide_wrapper .jsSlotMarker[data-slot-id="storefront4-campaign3-row1"]').before('<div class="promos"><div class="bottom"></div></div><div class="clear"></div>');
+				
+				$('#branded .content .wide_wrapper .promoMoveTop').prependTo('#branded .content .wide_wrapper .promos');
+				$('#branded .content .wide_wrapper .promoMoveBottom').appendTo('#branded .content .wide_wrapper .promos .bottom');
+			},
+
 			centerBoxFive : function () {
 				var boxFiveContent = $('#branded .content .wide_wrapper .products .box.five div');
 				var boxFive = $('#branded .content .wide_wrapper .products .box.five');
@@ -33,6 +40,7 @@ var jjShizzle = (function (jQuery) {
 })(jQuery);
 
 jQuery(document).ready(function () {
+	jjShizzle.wrapPromos();
 	jjShizzle.centerBoxFive();
 	jjShizzle.brandImgHover();
 	jjShizzle.promoHover();
