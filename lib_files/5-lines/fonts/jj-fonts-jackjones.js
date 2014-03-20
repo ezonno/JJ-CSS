c142de81a3c4c9ff590823a60ae12588f11565a6
jeppeolesen <jeppe.olesen@bestseller.com>
2014-03-20 16:42:02 +0100

    Redeployed fonts for all brands
    Close #36

--- lib_files/5-lines/fonts/jj-fonts-jackjones.js
+++ lib_files/5-lines/fonts/jj-fonts-jackjones.js
@@ -1,6 +1,6 @@
 /*
 
- MyFonts Webfont Build ID 2760786, 2014-03-04T08:12:53-0500
+ MyFonts Webfont Build ID 2774200, 2014-03-20T11:28:07-0400
 
  The fonts listed in this notice are subject to the End User License
  Agreement(s) entered into by the website owner. All other parties are 
@@ -10,32 +10,29 @@
 
  Webfont: PF Din Mono Medium by Parachute
  URL: http://www.myfonts.com/fonts/parachute/pf-din-mono/medium/
- Licensed pageviews: 15,000,000
 
  Webfont: PF Din Mono by Parachute
  URL: http://www.myfonts.com/fonts/parachute/pf-din-mono/regular/
- Licensed pageviews: 15,000,000
 
  Webfont: PF Din Mono Light by Parachute
  URL: http://www.myfonts.com/fonts/parachute/pf-din-mono/light/
- Licensed pageviews: 16,500,000
 
  Webfont: PF Din Mono Bold by Parachute
  URL: http://www.myfonts.com/fonts/parachute/pf-din-mono/bold/
+
+
+ License: http://www.myfonts.com/viewlicense?type=web&buildid=2774200
  Licensed pageviews: 15,000,000
-
-
- License: http://www.myfonts.com/viewlicense?type=web&buildid=2760786
  Webfonts copyright: Copyright (c) 2010 Parachute&#x00A8;, www.parachute.gr.  All rights reserved.
 
  ? 2014 MyFonts Inc
 */
-var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/2a2052";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0;
+var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/2a54b8";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0;
 if("undefined"!=typeof customPath)var path=customPath;else{var scripts=document.getElementsByTagName("SCRIPT"),script=scripts[scripts.length-1].src;script.match("://")||"/"==script.charAt(0)||(script="./"+script);path=script.replace(/\\/g,"/").replace(/\/[^\/]*\/?$/,"")}
 var wfpath=path+"/webfonts/",browsers=[{regex:"MSIE (\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:9,type:"woff"},{version:5,type:"eot"}]},{regex:"Trident/(\\d+\\.\\d+); (.+)?rv:(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$3)",type:[{version:11,type:"woff"}]},{regex:"Firefox[/s](\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:3.6,type:"woff"},{version:3.5,type:"ttf"}]},{regex:"Chrome/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:6,type:"woff"},
 {version:4,type:"ttf"}]},{regex:"Mozilla.*Android (\\d+\\.\\d+).*AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$1)",type:[{version:4.1,type:"woff"},{version:3.1,type:"svg#wf"},{version:2.2,type:"ttf"}]},{regex:"Mozilla.*(iPhone|iPad).* OS (\\d+)_(\\d+).* AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$2) + (new Number(RegExp.$3) / 10)",unhinted:!0,type:[{version:5,type:"woff"},{version:4.2,type:"ttf"},{version:1,type:"svg#wf"}]},{regex:"Mozilla.*(iPhone|iPad|BlackBerry).*AppleWebKit.*Safari",
 versionRegex:"1.0",type:[{version:1,type:"svg#wf"}]},{regex:"Version/(\\d+\\.\\d+)(\\.\\d+)? Safari/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:5.1,type:"woff"},{version:3.1,type:"ttf"}]},{regex:"Opera/(\\d+\\.\\d+)(.+)Version/(\\d+\\.\\d+)(\\.\\d+)?",versionRegex:"new Number(RegExp.$3)",type:[{version:11.1,type:"woff"},{version:10.1,type:"ttf"}]}],browLen=browsers.length,suffix="",i=0;
 a:for(;i<browLen;i++){var regex=RegExp(browsers[i].regex);if(regex.test(navigator.userAgent)){browserVersion=eval(browsers[i].versionRegex);var typeLen=browsers[i].type.length;for(j=0;j<typeLen;j++)if(browserVersion>=browsers[i].type[j].version&&(!0==browsers[i].unhinted&&(suffix="_unhinted"),webfontType=browsers[i].type[j].type,"woff"!=webfontType||woffEnabled)&&("svg#wf"!=webfontType||svgEnabled))break a}else webfontType="woff"}
 /(Macintosh|Android)/.test(navigator.userAgent)&&"svg#wf"!=webfontType&&(suffix="_unhinted");var head=document.getElementsByTagName("head")[0],stylesheet=document.createElement("style");stylesheet.setAttribute("type","text/css");head.appendChild(stylesheet);
-for(var fonts=[{fontFamily:"PFDinMono-Medium",url:wfpath+"2A2052_0"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Regular",url:wfpath+"2A2052_1"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Light",url:wfpath+"2A2052_2"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Bold",url:wfpath+"2A2052_3"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+
+for(var fonts=[{fontFamily:"PFDinMono-Medium",url:wfpath+"2A54B8_0"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Regular",url:wfpath+"2A54B8_1"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Light",url:wfpath+"2A54B8_2"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Bold",url:wfpath+"2A54B8_3"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+
 '")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&(css+="font-weight: "+fonts[i].fontWeight+";");fonts[i].fontStyle&&(css+="font-style: "+fonts[i].fontStyle+";");css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
--- lib_files/5-lines/fonts/jj-fonts-premium.js
+++ lib_files/5-lines/fonts/jj-fonts-premium.js
@@ -1,6 +1,6 @@
 /*
 
- MyFonts Webfont Build ID 2708770, 2013-12-16T04:38:53-0500
+ MyFonts Webfont Build ID 2774185, 2014-03-20T11:10:21-0400
 
  The fonts listed in this notice are subject to the End User License
  Agreement(s) entered into by the website owner. All other parties are 
@@ -13,8 +13,13 @@
  Copyright: Copyright (c) 2010 Parachute&#x00A8;, www.parachute.gr.  All rights reserved.
  Licensed pageviews: 15,000,000
 
- Webfont: PF Din Mono Regular by Parachute
+ Webfont: PF Din Mono by Parachute
  URL: http://www.myfonts.com/fonts/parachute/pf-din-mono/regular/
+ Copyright: Copyright (c) 2010 Parachute&#x00A8;, www.parachute.gr.  All rights reserved.
+ Licensed pageviews: 15,000,000
+
+ Webfont: PF Din Mono Light by Parachute
+ URL: http://www.myfonts.com/fonts/parachute/pf-din-mono/light/
  Copyright: Copyright (c) 2010 Parachute&#x00A8;, www.parachute.gr.  All rights reserved.
  Licensed pageviews: 15,000,000
 
@@ -49,17 +54,17 @@
  Licensed pageviews: 8,000,000
 
 
- License: http://www.myfonts.com/viewlicense?type=web&buildid=2708770
+ License: http://www.myfonts.com/viewlicense?type=web&buildid=2774185
 
- ? 2013 MyFonts Inc
+ ? 2014 MyFonts Inc
 */
-var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/295522";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0;
+var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/2a54a9";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0;
 if("undefined"!=typeof customPath)var path=customPath;else{var scripts=document.getElementsByTagName("SCRIPT"),script=scripts[scripts.length-1].src;script.match("://")||"/"==script.charAt(0)||(script="./"+script);path=script.replace(/\\/g,"/").replace(/\/[^\/]*\/?$/,"")}
 var wfpath=path+"/webfonts/",browsers=[{regex:"MSIE (\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:9,type:"woff"},{version:5,type:"eot"}]},{regex:"Trident/(\\d+\\.\\d+); (.+)?rv:(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$3)",type:[{version:11,type:"woff"}]},{regex:"Firefox[/s](\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:3.6,type:"woff"},{version:3.5,type:"ttf"}]},{regex:"Chrome/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:6,type:"woff"},
 {version:4,type:"ttf"}]},{regex:"Mozilla.*Android (\\d+\\.\\d+).*AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$1)",type:[{version:4.1,type:"woff"},{version:3.1,type:"svg#wf"},{version:2.2,type:"ttf"}]},{regex:"Mozilla.*(iPhone|iPad).* OS (\\d+)_(\\d+).* AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$2) + (new Number(RegExp.$3) / 10)",unhinted:!0,type:[{version:5,type:"woff"},{version:4.2,type:"ttf"},{version:1,type:"svg#wf"}]},{regex:"Mozilla.*(iPhone|iPad|BlackBerry).*AppleWebKit.*Safari",
 versionRegex:"1.0",type:[{version:1,type:"svg#wf"}]},{regex:"Version/(\\d+\\.\\d+)(\\.\\d+)? Safari/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:5.1,type:"woff"},{version:3.1,type:"ttf"}]},{regex:"Opera/(\\d+\\.\\d+)(.+)Version/(\\d+\\.\\d+)(\\.\\d+)?",versionRegex:"new Number(RegExp.$3)",type:[{version:11.1,type:"woff"},{version:10.1,type:"ttf"}]}],browLen=browsers.length,suffix="",i=0;
 a:for(;i<browLen;i++){var regex=RegExp(browsers[i].regex);if(regex.test(navigator.userAgent)){browserVersion=eval(browsers[i].versionRegex);var typeLen=browsers[i].type.length;for(j=0;j<typeLen;j++)if(browserVersion>=browsers[i].type[j].version&&(!0==browsers[i].unhinted&&(suffix="_unhinted"),webfontType=browsers[i].type[j].type,"woff"!=webfontType||woffEnabled)&&("svg#wf"!=webfontType||svgEnabled))break a}else webfontType="woff"}
 /(Macintosh|Android)/.test(navigator.userAgent)&&"svg#wf"!=webfontType&&(suffix="_unhinted");var head=document.getElementsByTagName("head")[0],stylesheet=document.createElement("style");stylesheet.setAttribute("type","text/css");head.appendChild(stylesheet);
-for(var fonts=[{fontFamily:"PFDinMono-Medium",url:wfpath+"295522_0"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Regular",url:wfpath+"295522_1"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Bold",url:wfpath+"295522_2"+suffix+"_0."+webfontType},{fontFamily:"AGaramond-Regular",url:wfpath+"295522_3"+suffix+"_0."+webfontType},{fontFamily:"AGaramondAlt-Regular",url:wfpath+"295522_4"+suffix+"_0."+webfontType},{fontFamily:"AGaramondExp-Regular",url:wfpath+"295522_5"+suffix+"_0."+webfontType},{fontFamily:"GillSansMTStd-Medium",
-url:wfpath+"295522_6"+suffix+"_0."+webfontType},{fontFamily:"GillSansMTStd-Book",url:wfpath+"295522_7"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+'")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&(css+="font-weight: "+fonts[i].fontWeight+";");fonts[i].fontStyle&&(css+="font-style: "+
-fonts[i].fontStyle+";");css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
+for(var fonts=[{fontFamily:"PFDinMono-Medium",url:wfpath+"2A54A9_0"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Regular",url:wfpath+"2A54A9_1"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Light",url:wfpath+"2A54A9_2"+suffix+"_0."+webfontType},{fontFamily:"PFDinMono-Bold",url:wfpath+"2A54A9_3"+suffix+"_0."+webfontType},{fontFamily:"AGaramond-Regular",url:wfpath+"2A54A9_4"+suffix+"_0."+webfontType},{fontFamily:"AGaramondAlt-Regular",url:wfpath+"2A54A9_5"+suffix+"_0."+webfontType},{fontFamily:"AGaramondExp-Regular",
+url:wfpath+"2A54A9_6"+suffix+"_0."+webfontType},{fontFamily:"GillSansMTStd-Medium",url:wfpath+"2A54A9_7"+suffix+"_0."+webfontType},{fontFamily:"GillSansMTStd-Book",url:wfpath+"2A54A9_8"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+'")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&
+(css+="font-weight: "+fonts[i].fontWeight+";");fonts[i].fontStyle&&(css+="font-style: "+fonts[i].fontStyle+";");css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
