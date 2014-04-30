5e6fc76b01337842a1d27e339d76da2f48f42fab
jeppeolesen <jeppe.olesen@bestseller.com>
2014-04-30 09:57:07 +0200

    Init roskilde stuff

---
+++ static_files/js/roskilde.js
@@ -0,0 +1,19 @@
+var jjRoskilde = (function (jQuery) {
+    if (!jQuery) {
+        alert(app.resources["MISSING_LIB"]);
+        return null;
+    }
+
+    if (!window.console) console = {log: function() {}};
+
+    return {
+
+        // custom functions here
+            
+        // end custom functions
+    };
+})(jQuery);
+
+jQuery(document).ready(function () {
+
+});
