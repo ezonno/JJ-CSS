9a06571801b3584a31b3321462cbdac827891c5b
Ash Lockmun <a.lockmun@gmail.com>
2014-10-24 15:13:09 +0200

    Navigation bug fixes

--- static_files/js/jj-navigation.js
+++ static_files/js/jj-navigation.js
@@ -1,9 +1,51 @@
-$( document ).ready(function() {
-   		// Click animation on simple_search
-		$('.simple_search').click(function(){
-				$('.simple_search').addClass("flash");
-				var delay = setTimeout(function() {
-				$('.simple_search').removeClass("flash");
-			}, 175)
-		});
-  });
+var jjNavigation = (function (jQuery) {
+    if (!jQuery) {
+        alert(app.resources["MISSING_LIB"]);
+        return null;
+    }
+	
+    if (!window.console) console = {log: function() {}};
+
+    return {
+
+        // custom functions here
+            scrollbar : function () {
+                var sideMenuCategories = $('#branded .side-menu-categories');
+                sideMenuCategories.perfectScrollbar({
+                    suppressScrollX: true,
+                    includePadding: true,
+                    wheelSpeed: 30,
+                    useKeyboard: false
+                });
+            },
+			
+			tabletscroll : function () {
+				if( jQuery('html').hasClass("touch") ) {
+					jQuery("#branded .side-menu-categories").perfectScrollbar('destroy');
+					jQuery("#branded .side-menu-categories").css("overflowY", "auto");
+					jQuery("#branded .side-menu-categories").css("position", "fixed", "important");
+					jQuery("#branded .side-menu .jj-j-shop-highlights").css("padding-top", "25px");
+				};
+			},
+			
+			// simplesearch flash on click	
+			simplesearchflash : function () {
+				jQuery('.simple_search').click(function(){
+					jQuery('.simple_search').addClass("flash");
+ 
+					var $delay = setTimeout(function() {
+						jQuery('.simple_search').removeClass("flash");
+					}, 175);
+				});
+			},
+
+        // end custom functions
+    };
+})(jQuery);
+
+jQuery(document).ready(function () {
+    jjNavigation.scrollbar();
+	jjNavigation.tabletscroll();
+	jjNavigation.simplesearchflash();
+});
+
